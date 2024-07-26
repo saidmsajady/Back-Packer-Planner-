de91e3313e4f4e6026e0d5b44e1492b032b84555
Said Masih <saidmsajady@gmail.com>
2024-07-26 10:57:54 -0700

    Fixed the css styling where the navbar covered the top list

--- Front-End/src/pages/Home/Home.jsx
+++ Front-End/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './Home.css'; // Ensure you import the CSS file
 
 const Home = () => {
   const [trips, setTrips] = useState([]);
